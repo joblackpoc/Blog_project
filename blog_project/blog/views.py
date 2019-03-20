93cc17e83a9ded20a38704297b959ac6c367e668
joblackpoc <duopoc@gmail.com>
2019-03-20 11:05:52 +0700

    dictionary

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 
+posts = [
+  {
+    'author': 'CoreyMS',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'March 18,2019'
+  },
+  {
+    'author': 'Joblack',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'March 19,2019'
+  },
+  {
+    'author': 'CoreyMS',
+    'title': 'Blog Post 3',
+    'content': 'Third post content',
+    'date_posted': 'March 20,2019'
+  }
+]
+
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html')
