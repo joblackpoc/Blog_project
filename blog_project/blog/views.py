921550853351f491fdbc4484409cf45e80b757ed
joblackpoc <duopoc@gmail.com>
2019-03-27 16:33:42 +0700

    post detail

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 def home(request):
   context = {
     'posts': Post.objects.all()
   }
-  return render(request, 'blog/home.html', context)
+  return render(request, 'blog/home.html', context) #<app>/<model>_<viewtype>.html 
 
 class PostListView(ListView):
   model = Post
   template_name = 'blog/home.html'
   context_object_name = 'posts'
-  ordering = ['-date_posted']# DESC use -
+  # DESC use -
+  ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+  model = Post
+
+
 
 def about(request):
   return render(request, 'blog/about.html', {'title': 'About'})
